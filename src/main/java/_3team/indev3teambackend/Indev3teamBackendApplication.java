400332fcbe8efa65ec56d85349c4789f27309d43
HIATK <githubarchive10@gmail.com>
2025-09-08 17:51:40 +0900

    firstUpload

---
+++ src/main/java/_3team/indev3teambackend/Indev3teamBackendApplication.java
@@ -0,0 +1,13 @@
+package _3team.indev3teambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Indev3teamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Indev3teamBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/_3team/indev3teambackend/Indev3teamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package _3team.indev3teambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Indev3teamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
